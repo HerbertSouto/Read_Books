1a8454a5697c24534494ac30373520b8a4fceda2
Herbert Souto <herbert-93@hotmail.com>
2024-07-31 19:15:47 -0300

    Atualizando banco para PostgreeSQL no Render

--- Backend/database.py
+++ Backend/database.py
@@ -1,8 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-POSTGRES_DATABASE_URL = "postgresql://user:password@postgres/mydatabase"
+# Carregar variáveis de ambiente do arquivo .env
+load_dotenv()
+
+# Configurações da conexão
+host_name = os.getenv('HOST_NAME')
+database_name = os.getenv('DATABASE_NAME')
+user_name = os.getenv('USER_NAME')
+password = os.getenv('PASSWORD')
+
+POSTGRES_DATABASE_URL = f"postgresql://{user_name}:{password}@{host_name}/{database_name}"
 
 engine = create_engine(POSTGRES_DATABASE_URL)
 
