3426767f223d4c9749f28ed8c61011213f104987
Herbert Souto <herbert-93@hotmail.com>
2024-07-31 21:40:38 -0300

    teste no front

--- Frontend/app.py
+++ Frontend/app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import requests
 import pandas as pd
 
-st.set_page_config(layout="wide")
-st.image("logo.png", width=100)
 st.title("Gerenciamento de Livros")
 
 # Função auxiliar para exibir mensagens de erro detalhadas
